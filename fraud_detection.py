bf84b2d795697104d0e03738eb679fa35f6f6372
Vaibhav <vaibhavvvv1@gmail.com>
2026-04-13 00:35:43 +0530

    Update fraud_detection.py

--- fraud_detection.py
+++ fraud_detection.py
@@ -6,6 +6,7 @@ model=joblib.load("fraud_detection_pipeline.pkl")
 st.title("Fraud Detection Prediction App")
 st.markdown("Please enter the transaction details and use the predict button")
 st.divider()
+
 transaction_type=st.selectbox("Transaction Type",["PAYMENT","TRANSFER","CASH_OUT","DEPOSIT"])
 amount=st.number_input("AMOUNT",min_value=0.0,value=1000.0)
 oldbalanceOrg=st.number_input("Old Balance(Sender)",min_value=0.0,value=10000.0)
